83b0f159d3ea37ff565950af562cc15f3f51738b
Ella Wang <ella.xi.wang@gmail.com>
2021-03-18 23:39:42 +1100

    Add SpecAnalysis class and tests Moved some functions into this class, added other functions for spectral analysis.

--- astro_tools.py
+++ astro_tools.py
@@ -1,7 +1,187 @@
 import numpy as np
+from scipy.stats.mstats import theilslopes
 
 # define constants
 _c = 299792.458 # speed of light in km s^-1
+
+
+class SpecAnalysis:
+    '''Analyse astronomy spectra.
+    '''
+
+    def __init__(self, wavelength, flux, flux_err=None):
+        '''
+        Parameters
+        ----------
+        wavelength : List[Real] or 1darray
+            Input wavelengths. Needs to be monotonically increasing.
+        flux : List[Real] or 1darray
+            Input flux profile.
+        flux_err : List[Real] or 1darray
+            Input flux error.
+        '''
+
+        # if no error, then set error to 0
+        if flux_err is None:
+            self.flux_err = np.full(len(flux), 0)
+        
+        # change to numpy array
+        try: 
+            self.wl = np.array(wavelength, dtype=float)
+            self.flux = np.array(flux, dtype=float)
+            self.flux_err = np.array(flux_err, dtype=float)
+        except:
+            raise ValueError('Could not turn input into numpy arrays.')
+
+        # check monotonic increasing wavelength
+        if not ((self.wl[1:] - self.wl[:-1]) > 0).all():
+            raise ValueError('Wavelength needs to be strictly increasing')
+
+        # check length of all input equal
+        if not len(self.wl) == len(self.flux) == len(self.flux_err):
+            raise ValueError(
+        'wavelength, flux, flux_err are not the same length')
+
+    def save(self, wl, flux, flux_err):
+        '''Save the values given into the class
+        '''
+
+        self.wl = wl
+        self.flux = flux
+        self.flux_err = flux_err
+
+    def mask_region(self, masks, mtype='out'):
+        '''Mask (remove) a region of the spectrum. 
+
+        Parameters
+        ----------
+        masks : list of lists[float]
+            The regions which you want to mask.
+        mtype : str, optional
+            To remove the region in the mask or out. Accepted values: 'in' and 
+            'out'.
+
+        Returns
+        -------
+        wl, flux, flux_err : 3darray
+            Masked wavelength, flux, and flux error.
+        '''
+
+        # make mask
+        mask_full = np.ones(len(self.wl), dtype=bool)
+        for lower, upper in masks:
+            mask = (lower <= self.wl) & (self.wl <= upper)
+            if mtype == 'in':
+                mask = ~mask
+            mask_full = mask_full & mask
+
+        # apply mask
+        self.save(
+            self.wl[mask_full], self.flux[mask_full], self.flux_err[mask_full]
+        )
+
+        return self.wl, self.flux, self.flux_err
+
+    def cut(self, center=670.9659, upper=10, lower=10, rtype='wl'):
+        '''Cuts the wavelength, flux, and flux error and returns the values 
+        between center - lower and center + upper.
+
+        Parameters
+        ----------
+        center : Real, optional
+            The center of the wavelengths where the cut should be taken, in the
+            same units as the wavelength. 
+        upper : Positive Real, optional
+            The amount to go above the center when taking the cut, in the same 
+            units of nm if rtype=wl, or in km/s if rtype=vr.
+        lower : Positive Real, optional
+            The amount to go below the center when taking the cut, in the same 
+            units of nm if rtype=wl, or in km/s if rtype=vr.
+        rtype : str, optional
+            The type upper and lower is in. Either wl or vr (wavelength, 
+            radial velocity respectively).
+
+        Returns
+        -------
+        wl, flux, flux_err : 3darray
+            Cut wavelength, flux, and flux error.
+        '''
+
+        # convert to wavelength
+        if rtype == 'vr':
+            lower = vr_to_wl(lower, center=center)
+            upper = vr_to_wl(upper, center=center) 
+
+        # cut
+        low = center - lower
+        high = center + upper
+        self.mask_region([[low, high]])
+
+        return self.wl, self.flux, self.flux_err
+
+    def sigma_clip(self, func, args=(), sigma_cut=3, iterations=1):
+        '''clip outliers based on a sigma cut.
+
+        Parameters
+        ----------
+        func : callable ``func(self.wl, self.flux, self.flux_err, *args)``.
+            The function to fit the spectrum to. 
+        args : tuple, optional
+            Extra arguments passed to func, 
+            i.e., ``func(self.wl, self.flux, self.flux_err, *args)``.
+        sigma_cut : float
+            The tolerance on sigma clip.
+        iterations : int
+            The number of times to iterate the sigma clip.
+
+        Returns
+        -------
+        wl, flux, flux_err : 3darray
+            Clipped wavelength, flux, and flux error.
+        '''
+
+        for _ in range(iterations):
+            flux_fit = func(self.wl, self.flux, self.flux_err, *args)
+            diff = np.abs(self.flux - flux_fit)
+            sigma = np.std(diff)
+            mask = diff < sigma*sigma_cut
+            self.save(self.wl[mask], self.flux[mask], self.flux_err[mask])
+        return self.wl, self.flux, self.flux_err
+
+    def cont_norm(self, center, mask_step=0.01, sigma_cut=3, iterations=3):
+        '''Normalise the continuum for the flux and flux_error. Returns flux and flux_err.
+        '''
+
+        # save original values
+        wl = self.wl
+        flux = self.flux
+        flux_err = self.flux_err
+        
+        # first do a shitty normalisation with the main line removed
+        masks = [[center - mask_step, center + mask_step]]
+        self.mask_region(masks, mtype='in')
+        med = np.median(self.flux)
+        flux = flux/med
+        flux_err = flux_err/med
+
+        # sigma clip 
+        self.save(wl, flux, flux_err)
+        median = lambda x,y,z:np.median(y)
+        self.sigma_clip(median, sigma_cut=sigma_cut, iterations=iterations)
+
+        # fit
+        fit = theilslopes(self.flux, self.wl)
+        grad = fit[0]
+        intercept = fit[1]
+
+        # remove linear slope
+        fit = wl*grad + intercept
+        flux = flux/fit
+        flux_err = flux_err/fit
+        self.save(wl, flux, flux_err)
+
+        return self.wl, self.flux, self.flux_err
+
 
 def polyfit(x, y, x_out=None, deg=1):
     '''Fits a polynomial to input data after shifting data to 0.
@@ -19,8 +199,8 @@ def polyfit(x, y, x_out=None, deg=1):
 
     Returns
     -------
-    y_out : 1darray
-        Output y values at the input x_out values. 
+    y_out or center, fit: 1darray or tuple(float)
+        Output y values at the input x_out values or x mean, grad, intercept.
     '''
 
     # convert to numpy arrays
@@ -30,6 +210,12 @@ def polyfit(x, y, x_out=None, deg=1):
     # fit
     center_x = np.mean(x)
     fit = np.polyfit(x - center_x, y, deg=deg)
+
+    # return fit and center
+    if x_out is None:
+        return center_x, fit[0], fit[1]
+    
+    # return y evaluated at x_out
     y_out = np.polyval(fit, x_out - center_x)
     return y_out
 
@@ -58,38 +244,6 @@ def cut_wavelength(wavelength, center=670.9659, upper=10, lower=10):
     high = center + upper
     wl_cut = wavelength[(low <= wavelength) & (high >= wavelength)]
     return wl_cut
-
-def cut(wavelength, line_profile, center=670.9659, upper=10, lower=10):
-    """Cuts the wavelength and line profile and returns the values between center - lower and center + upper.
-    Parameters
-    ----------
-    wavelength : List[Real] or 1darray
-        Input wavelengths. Needs to be monotonically increasing.
-    line_profile : List[Real] or 1darray
-        Input line profile.
-    center : Real, optional
-        The center of the wavelengths where the cut should be taken, in the same units as the wavelength. 
-    upper : Positive Real, optional
-        The amount to go above the center when taking the cut, in the same units as the wavelength.
-    lower : Positive Real, optional
-        The amount to go below the center when taking the cut, in the same units as the wavelength.
-
-    Returns
-    -------
-    cut_data : 2darray
-        Cut wavelengths and line profiles.
-    """
-
-    wavelength = np.array(wavelength)
-    line_profile = np.array(line_profile)
-
-    low = center - lower
-    high = center + upper
-    mask = (low <= wavelength) & (high >= wavelength)
-    wl_cut = wavelength[mask]
-    line_cut = line_profile[mask]
-    cut_data = np.array([wl_cut, line_cut])
-    return cut_data
 
 def wl_to_vr(wl, center=670.9659):
     '''Converts wavelengths to radial velocity, works for errors too.
@@ -170,31 +324,3 @@ def air_to_vac(lam):
     s = 1e4/lam
     n = 1 + 0.00008336624212083 + 0.02408926869968 / (130.1065924522 - s**2) + 0.0001599740894897 / (38.92568793293 - s**2)
     return lam*n
-
-def sigma_clip(x, y, y_pred, sigma_cut=3):
-    '''clip outliers based on a sigma cut.
-
-    Parameters
-    ----------
-    x : list or 1darray
-        x values.
-    y : list or 1darray 
-        y values. 
-    y_pred : list or 1darray
-        Predicted y values.
-    sigma_cut : float
-        The tolerance on sigma cut.
-
-    Returns
-    -------
-    (x, y) : (1darray, 1darray)
-        Clipped x and y values.
-    '''
-
-    x, y, y_pred = np.array([x, y, y_pred])
-    y_diff = np.abs(y - y_pred)
-    sigma = np.std(y_diff)
-    mask = y_diff < sigma*sigma_cut
-    x = x[mask]
-    y = y[mask]
-    return (x, y)
--- tests/test_astro_tools.py
+++ tests/test_astro_tools.py
@@ -7,6 +7,119 @@ def assert_array_eq(x, y):
     assert np.array_equal(x, y)
 
 
+class Test_SpecAnalysis(unittest.TestCase):
+    '''Test __init__ from SpecAnalysis.
+    '''
+
+    def test_np_array(self):
+        #TODO: test all of the valueerrors are raised correctly in the init
+        #TODO: test if all functions in this class with without flux_err
+        pass
+
+
+class Test_cut(unittest.TestCase):
+    '''Test mask_region and cut from SpecAnalysis.
+    '''
+
+    def test_empty(self):
+        assert_array_eq(SpecAnalysis([], [], []).cut(), np.array([[], [], []]))
+
+    def test_inc_edge(self):
+        cut = SpecAnalysis(
+            [1, 2, 3, 4], 
+            [4, 5, 6, 7], 
+            [0.1, 0.2, 0.1, 0.1]
+        ).cut(center=2, upper=1, lower=1)
+        assert_array_eq(cut, [[1, 2, 3], [4, 5, 6], [0.1, 0.2, 0.1]])
+
+    def test_rv(self):
+        cut = SpecAnalysis(
+            [650, 660, 671, 680, 690],
+            [1, 2, 3, 4, 5],
+            [0.1, 0.2, 0.2, 0.3, 0.1]
+        ).cut(lower=100, upper=100, rtype='vr')
+        assert_array_eq(cut, [[671], [3], [0.2]])
+
+    def test_case1(self):
+        cut = SpecAnalysis(
+            [660, 669, 671, 680, 690], 
+            [1, -3, -6, 0, 2], 
+            [0.1, 0.1, 0.2, 0.1, 0.1]
+        ).cut()
+        assert_array_eq(cut, [[669, 671, 680], [-3, -6, 0], [0.1, 0.2, 0.1]])
+
+    def test_case2(self):
+        cut = SpecAnalysis(
+            [810, 812, 813, 816, 818, 819], 
+            [0, 1, 2, 3, 4, 5], 
+            [0.1, 0.2, 0.1, 0.1, 0.1, 0.1]
+        ).cut(center=812.8606, upper=1, lower=2)
+        assert_array_eq(cut, [[812, 813], [1, 2], [0.2, 0.1]])
+
+    def test_case3(self):
+        cut = SpecAnalysis(
+            [608, 609, 610, 612, 614], 
+            [-9, 1, 203, 40, 1], 
+            [0.1, 0.1, 0.1, 0.2, 0.1]
+        ).cut(center = 610.5298, upper = 3, lower = 0)
+        assert_array_eq(cut, [[612], [40], [0.2]])
+
+
+class TestSigmaClip(unittest.TestCase):
+    '''Test sigma_clip from SpecAnalysis.
+    '''
+
+    def test_mean(self):
+        clip = SpecAnalysis(
+            [1, 2, 3, 4, 5, 6], 
+            [0.1, 0.2, 0.1, -0.1, 100, -0.2], 
+            [0.1, 0.1, 0.1, 0.1, 10, 0.1]
+        ).sigma_clip(lambda x, y, z: np.mean(y), sigma_cut=3)
+        # mean will be more affected by outliers, so std will be lower
+        # which means sigma_cut can be higher to remove outlier
+        assert_array_eq(clip, [
+            [1, 2, 3, 4, 6], 
+            [0.1, 0.2, 0.1, -0.1, -0.2], 
+            [0.1, 0.1, 0.1, 0.1, 0.1]
+            ])
+
+    def test_median(self):
+        clip = SpecAnalysis(
+            [1, 2, 3, 4, 5, 6], 
+            [0.1, 0.2, 0.1, -0.1, 100, -0.2], 
+            [0.1, 0.1, 0.1, 0.1, 10, 0.1]
+        ).sigma_clip(lambda x, y, z: np.median(y), sigma_cut=2)
+        # median will be more resistant to outliers, so std will be higher
+        # which means sigma_cut needs to be smaller to remove outlier
+        assert_array_eq(clip, [
+            [1, 2, 3, 4, 6], 
+            [0.1, 0.2, 0.1, -0.1, -0.2], 
+            [0.1, 0.1, 0.1, 0.1, 0.1]
+            ])
+
+    def test_func_iter(self):
+        #TODO: implement something that will test a poly function fit
+        pass
+
+
+class TestContNorm(unittest.TestCase):
+    '''Test cont_norm from SpecAnalysis.
+    '''
+
+    def test_case1(self):
+        norm = SpecAnalysis(
+            [1, 2, 3, 4, 5, 6, 7], 
+            [1, 1.01, 1.02, 0.03, 1.04, 1.05, 1.06], 
+            [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        ).cont_norm(4, sigma_cut=1, iterations=1)
+        expected = np.array(
+            [[1, 2, 3, 4, 5, 6, 7], 
+            [1, 1, 1, 0, 1, 1, 1], 
+            [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]], dtype=float
+        )
+        self.assertTrue(np.allclose(norm, expected, atol=1e-1))
+
+
 class TestPolyfit(unittest.TestCase):
     '''Test polyfit function
     '''
@@ -19,7 +132,7 @@ class TestPolyfit(unittest.TestCase):
         y = np.arange(0, 10, 1)
         y_out = polyfit(x, y, x, deg=1)
 
-        np.allclose(y, y_out)
+        self.assertTrue(np.allclose(y, y_out))
 
     def test_extreme(self):
         '''Test that polyfit shifts to center and fits correctly. Without shifting
@@ -33,6 +146,14 @@ class TestPolyfit(unittest.TestCase):
         y_out = polyfit(x, y, x_out, deg=deg)
 
         self.assertTrue(np.sum(np.square(np.array(y) - y_out)) < 1e-5)
+
+    def test_out_fit(self):
+        x = [1, 2, 3]
+        y = [1, 2, 3]
+        center_x, grad, inter = polyfit(x, y, deg=1)
+        self.assertTrue(np.abs(center_x - 2) < 1e-5)
+        self.assertTrue(np.abs(grad - 1) < 1e-5)
+        self.assertTrue(np.abs(inter - 2) < 1e-5)
 
 
 class Test_cut_wavelength(unittest.TestCase):
@@ -55,26 +176,6 @@ class Test_cut_wavelength(unittest.TestCase):
         assert_array_eq(cut_wavelength([608, 609, 610, 612, 614], center = 610.5298, upper = 3, lower = 0), [612])
 
 
-class Test_cut(unittest.TestCase):
-    '''Test cut function.
-    '''
-
-    def test_empty(self):
-        assert_array_eq(cut([], []), np.array([[], []]))
-
-    def test_inc_edge(self):
-        assert_array_eq(cut([1, 2, 3, 4], [4, 5, 6, 7], center = 2, upper = 1, lower = 1), [[1, 2, 3], [4, 5, 6]])
-
-    def test_case1(self):
-        assert_array_eq(cut([660, 669, 671, 680, 690], [1, -3, -6, 0, 2]), [[669, 671, 680], [-3, -6, 0]])
-
-    def test_case2(self):
-        assert_array_eq(cut([810, 812, 813, 816, 818, 819], [0, 1, 2, 3, 4, 5], center = 812.8606, upper = 1, lower = 2), [[812, 813], [1, 2]])
-
-    def test_case3(self):
-        assert_array_eq(cut([608, 609, 610, 612, 614], [-9, 1, 203, 40, 1], center = 610.5298, upper = 3, lower = 0), [[612], [40]])
-
-
 class TestWlVr(unittest.TestCase):
     '''Test wl_to_vr and vr_to_wl functions.
     '''
@@ -87,7 +188,7 @@ class TestWlVr(unittest.TestCase):
         wl = np.array([600, 700, 800])
         center = 600
         vr = wl_to_vr(wl, center=center)
-        np.allclose(vr_to_wl(vr, center=center), wl)
+        self.assertTrue(np.allclose(vr_to_wl(vr, center=center), wl))
 
     def test_single(self):
         '''Test that converting from wl to vr and back gives the same results
@@ -111,7 +212,7 @@ class TestAirVac(unittest.TestCase):
 
         wl = np.array([6000, 7000, 8000])
         air = vac_to_air(wl)
-        np.allclose(air_to_vac(air), wl)
+        self.assertTrue(np.allclose(air_to_vac(air), wl))
 
     def test_single(self):
         '''Test that converting from vacuum to air and back gives the same 
